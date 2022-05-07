0997307bc19aa371a843caf0a5c321f918a54cf2
SdangondLT <sdangond@lean-tech.io>
2022-05-07 16:29:42 -0500

    added Recruiter Entity, their Core, Controller and DataAccess configurations.

--- RecruitmentManager.DataAccess/Context/SqlServerContext.cs
+++ RecruitmentManager.DataAccess/Context/SqlServerContext.cs
@@ -11,37 +11,28 @@ namespace RecruitmentManager.DataAccess.Context
     public class SqlServerContext: DbContext
     {
         private readonly string _connectionString = string.Empty;
+
         public DbSet<Client> Client { get; set; }
+        public DbSet<Recruiter> Recruiter { get; set; }
 
         public SqlServerContext()
         {
-            //un strin de connection es la ruta ala DB
             _connectionString = @"Data Source = DESKTOP-IF9J0OU\SQLEXPRESS; Initial Catalog = RecruitmentManager; Integrated Security = true; User Id=sa; Password=";
-
         }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            //servidor
-            //base de datos
-            //modo de seguridad de acceso
-            //usuario
-            //contrase;a
             optionsBuilder.UseSqlServer(_connectionString);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {//algunas conf de nuestras entidades
+        {
             modelBuilder.Entity<Client>().HasKey(c => new {c.IdClient} );
             modelBuilder.Entity<Client>().Property(c => c.IdClient).UseIdentityColumn().Metadata.SetBeforeSaveBehavior(Microsoft.EntityFrameworkCore.Metadata.PropertySaveBehavior.Ignore);
 
-            //modelBuilder.Entity<Client>()
-            //    .HasOne(c => c.Client)
-            //    .WithMany(c => c.Vacancy)
-            //    .HasForeignKey(c => c.IdClient);
+            modelBuilder.Entity<Recruiter>().HasKey(c => new { c.IdRecruiter });
+            modelBuilder.Entity<Recruiter>().Property(c => c.IdRecruiter).UseIdentityColumn().Metadata.SetBeforeSaveBehavior(Microsoft.EntityFrameworkCore.Metadata.PropertySaveBehavior.Ignore);
+            
             base.OnModelCreating(modelBuilder);
-
-        }
-
-        
+        }        
     }
 }
