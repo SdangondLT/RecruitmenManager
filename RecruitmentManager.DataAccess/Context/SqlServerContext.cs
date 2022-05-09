0cf5ed99af7383de5ab2e5a2ee3f99ba9323a2ec
Santiago Cardona <ssuarez@lean-tech.io>
2022-05-09 11:51:03 -0500

    Creating Assessment

--- RecruitmentManager.DataAccess/Context/SqlServerContext.cs
+++ RecruitmentManager.DataAccess/Context/SqlServerContext.cs
@@ -13,35 +13,31 @@ namespace RecruitmentManager.DataAccess.Context
         private readonly string _connectionString = string.Empty;
         public DbSet<Client> Client { get; set; }
 
+        public DbSet<Assessment> Assessment { get; set; }
+
         public SqlServerContext()
         {
-            //un strin de connection es la ruta ala DB
-            _connectionString = @"Data Source = DESKTOP-IF9J0OU\SQLEXPRESS; Initial Catalog = RecruitmentManager; Integrated Security = true; User Id=sa; Password=";
+            _connectionString = @"Data Source = DESKTOP-G2S8F9A\SQLEXPRESS; Initial Catalog = RecruitmentManager; Integrated Security = true; User Id=sa; Password=";
 
         }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            //servidor
-            //base de datos
-            //modo de seguridad de acceso
-            //usuario
-            //contrase;a
+
             optionsBuilder.UseSqlServer(_connectionString);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {//algunas conf de nuestras entidades
+        {
             modelBuilder.Entity<Client>().HasKey(c => new {c.IdClient} );
             modelBuilder.Entity<Client>().Property(c => c.IdClient).UseIdentityColumn().Metadata.SetBeforeSaveBehavior(Microsoft.EntityFrameworkCore.Metadata.PropertySaveBehavior.Ignore);
 
-            //modelBuilder.Entity<Client>()
-            //    .HasOne(c => c.Client)
-            //    .WithMany(c => c.Vacancy)
-            //    .HasForeignKey(c => c.IdClient);
+            modelBuilder.Entity<Assessment>().HasKey(a => new {a.IdAssessment});
+            modelBuilder.Entity<Assessment>().Property(c => c.IdAssessment).UseIdentityColumn().Metadata.SetBeforeSaveBehavior(Microsoft.EntityFrameworkCore.Metadata.PropertySaveBehavior.Ignore);
+
             base.OnModelCreating(modelBuilder);
 
         }
 
-        
+       
     }
 }
