8da0df6054502a385bc475cb61f1ea77275f96ff
IsaSanri <isanchez@lean-tech.io>
2022-05-09 11:17:15 -0500

    f

--- RecruitmentManager.DataAccess/Context/SqlServerContext.cs
+++ RecruitmentManager.DataAccess/Context/SqlServerContext.cs
@@ -16,31 +16,24 @@ namespace RecruitmentManager.DataAccess.Context
 
         public SqlServerContext()
         {
-            //un strin de connection es la ruta ala DB
+            
             _connectionString = @"Data Source = LTUSPE-L0004\SQLEXPRESS; Initial Catalog = RecruitmentManager; Integrated Security = true;";
 
         }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            //servidor
-            //base de datos
-            //modo de seguridad de acceso
-            //usuario
-            //contrase;a
+          
             optionsBuilder.UseSqlServer(_connectionString);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {//algunas conf de nuestras entidades
+        {
             modelBuilder.Entity<Client>().HasKey(c => new {c.IdClient} );
             modelBuilder.Entity<Client>().Property(c => c.IdClient).UseIdentityColumn().Metadata.SetBeforeSaveBehavior(Microsoft.EntityFrameworkCore.Metadata.PropertySaveBehavior.Ignore);
             modelBuilder.Entity<Candidate>().HasKey(c => new { c.IdCandidate });
             modelBuilder.Entity<Candidate>().Property(c => c.IdCandidate).UseIdentityColumn().Metadata.SetBeforeSaveBehavior(Microsoft.EntityFrameworkCore.Metadata.PropertySaveBehavior.Ignore);
 
-            //modelBuilder.Entity<Client>()
-            //    .HasOne(c => c.Client)
-            //    .WithMany(c => c.Vacancy)
-            //    .HasForeignKey(c => c.IdClient);
+           
             base.OnModelCreating(modelBuilder);
 
         }
